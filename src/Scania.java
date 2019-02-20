28eb0f2496a100250c92c34991ee9408450b3327
zinkdeveloper <rikard.viktig.nordahl@gmail.com>
2019-02-20 11:28:18 +0100

    Scania.java skapad

---
+++ src/Scania.java
@@ -0,0 +1,49 @@
+import javafx.scene.paint.Color;
+
+public class Scania extends Car {
+
+	private boolean bedActive;
+	private double bedAngle;
+	private double timer;
+	private double bedTimer;
+	
+	public Scania() {
+		super(Color.WHITE, 500, "Scania");
+		bedAngle = 70;
+		timer = System.currentTimeMillis();
+		bedTimer = 20;
+	}
+	/**
+	 * Tar upp eller ner flaket (gör det stegvis med hjälp av tid) 
+	 * @param arg om den ska åka upp eller inte 
+	 */
+	public void setTruckBed(boolean arg) {
+		bedActive = arg;
+		if (bedActive) {
+			while (bedAngle > 0) {
+				if (System.currentTimeMillis() - timer >= bedTimer) {
+					bedAngle-=0.5;
+					System.out.println(bedAngle);
+					timer = System.currentTimeMillis();
+				}
+			}
+		} else {
+			if (!bedActive) {
+				while (bedAngle < 70) {
+					if (System.currentTimeMillis() - timer >= bedTimer) {
+						bedAngle+=0.5;
+						timer = System.currentTimeMillis();
+					}
+				}
+			}
+		}
+	}
+	/**
+	 * Returnerar om flaket är nere eller inte
+	 * @return
+	 */
+	public double getBedAngle() {
+		return bedAngle;
+	}
+
+}
